e27cf7c5ff80281658cb8447ae75949fdb43f3e8
“Aria <a.c.nikzad@gmail.com>
2020-02-18 15:39:05 -0800

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,80 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-bathroom = Bathroom.create(park: "Airport Little League Park", latitude: 38.508932359829, longitude: -121.483599357993)
-bathroom.save
+# bathroom = Bathroom.create(park: "Airport Little League Park", latitude: 38.508932359829, longitude: -121.483599357993)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "Argonaut Park", latitude: 38.51822717043, longitude: -121.505121809116)
-bathroom.save
+# bathroom = Bathroom.create(park: "Argonaut Park", latitude: 38.51822717043, longitude: -121.505121809116)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "Earl Warren Park", latitude: 38.527262952937, longitude: -121.416895069333)
-bathroom.save
+# bathroom = Bathroom.create(park: "Earl Warren Park", latitude: 38.527262952937, longitude: -121.416895069333)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "Franklin Boyce Community Park", latitude: 38.478240312181, longitude: -121.452453434246)
-bathroom.save
+# bathroom = Bathroom.create(park: "Franklin Boyce Community Park", latitude: 38.478240312181, longitude: -121.452453434246)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "John Fremont Park", latitude: 38.570852677373, longitude: -121.489110243271)
-bathroom.save
+# bathroom = Bathroom.create(park: "John Fremont Park", latitude: 38.570852677373, longitude: -121.489110243271)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "George Sim Park", latitude: 38.513281321170300, longitude: -121.414827906638000)
-bathroom.save
+# bathroom = Bathroom.create(park: "George Sim Park", latitude: 38.513281321170300, longitude: -121.414827906638000)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "O'neil Field", latitude: 38.565932960766200, longitude: -121.504516970667000)
-bathroom.save
+# bathroom = Bathroom.create(park: "O'neil Field", latitude: 38.565932960766200, longitude: -121.504516970667000)
+# bathroom.save
 
-bathroom = Bathroom.create(park: "Margarette 'MAMA' Marks Park", latitude: 38.634681964793800, longitude: -121.44045382735900)
+# bathroom = Bathroom.create(park: "Margarette 'MAMA' Marks Park", latitude: 38.634681964793800, longitude: -121.44045382735900)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Redwood Park", latitude: 38.610279414091200, longitude: -121.465195736377000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "William Chorley Park", latitude: 38.497937913602400, longitude: -121.492588493131000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Bill Conlin Youth Sports Complex", latitude: 38.47106624646600, longitude: -121.501818754725000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "East Portal Park", latitude: 38.634681964793800, longitude: -121.44045382735900)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Garcia Bend Park", latitude: 38.478602967396900, longitude: -121.542041328892000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Gardenland Park", latitude: 38.617692791026100, longitude: 38.617692791026100)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Hagginwood Park", latitude: 38.629054347934900, longitude: 38.629054347934900)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Glenn Hall Park", latitude: 38.579577552783800, longitude: -121.426188858350000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Bertha Henschel Park", latitude: 38.576930043096700, longitude: -121.443520684396000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Jacinto Creek Park", latitude: 38.445465504786300, longitude: -121.407682293152000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "William Land Golf Course", latitude: 38.542276703358500, longitude: -121.501109833454000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Lawrence Park", latitude: 38.528469127032500, longitude: -121.450092782806000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "James Mangan Park", latitude: 38.521676332303000, longitude: -121.489400841040000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "C.K. McClatchy Park", latitude: 38.548151632278200, longitude: -121.469262399519000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "William McKinley Park", latitude: 38.577133253970500, longitude: -121.462667603077000)
+# bathroom.save
+
+# bathroom = Bathroom.create(park: "Meadowview Park", latitude: 38.474641392983400, longitude: -121.480931853365000)
+# bathroom.save
+
+
+
+
+bathroom = Bathroom.create(park: "Meadowview Park", latitude: 38.474641392983400, longitude: -121.480931853365000)
 bathroom.save
