a22b79932d38848bf8ec266e2bf9de94dfe44ca4
“Aria <a.c.nikzad@gmail.com>
2020-02-19 08:05:51 -0800

    finished DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -139,10 +139,10 @@ bathroom.save
 bathroom = Bathroom.create(park: "Leland Stanford Park", latitude: 38.568282583061600, longitude: -121.50378654881400)
 bathroom.save
 
-bathroom = Bathroom.create(park: "Tahoe Park", latitude: 38.542811580455800, longitude: -121.436270694728000)
+bathroom = Bathroom.create(park: "Tahoe Park (South West Corner)", latitude: 38.542811580455800, longitude: -121.436270694728000)
 bathroom.save
 
-bathroom = Bathroom.create(park: "South Natomas Community Park", latitude: 38.618708542241300, longitude: -121.499239187024000)
+bathroom = Bathroom.create(park: "South Natomas Community Park Library", latitude: 38.618708542241300, longitude: -121.499239187024000)
 bathroom.save
 
 bathroom = Bathroom.create(park: "Belle Cooledge Community Center Park", latitude: 38.52410456382670, longitude: -121.510795446551000)
@@ -157,5 +157,74 @@ bathroom.save
 bathroom = Bathroom.create(park: "Pannell/Meadowview Community Center Park", latitude: 38.48079025140130, longitude: -121.478837466451000)
 bathroom.save
 
-bathroom = Bathroom.create(park: "South Natomas Community Park", latitude: 38.619224487400700, longitude: -121.499310198925000)
+bathroom = Bathroom.create(park: "South Natomas Community Center", latitude: 38.619224487400700, longitude: -121.499310198925000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Oak Park", latitude: 38.543565899234000, longitude: -121.464088092149000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "James W. Marshall Park", latitude: 38.574854455446000, longitude: -121.469845315079000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Belle Cooledge Park", latitude: 38.524034175077800, longitude: -121.511954457742000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Shasta Community Park", latitude: 38.451714652296400, longitude: -121.415314859934000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Bartley Cavanaugh Golf Course", latitude: 38.451714652296400, longitude: -121.498977226177000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Pannell/Meadowview Community Center Park", latitude: 38.481044992385400, longitude: -121.478166509134000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Del Paso Regional Park", latitude: 38.63901999080580, longitude: -121.386850510257000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Sacramento Historic Old City Cemetery", latitude: 38.561417748772800, longitude: -121.501116428462000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Valley Oak Park", latitude: 38.674542082961900, longitude: -121.52818560340700)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William Land Regional Park (Sutterville)", latitude: 38.537741716164600, longitude: -121.502256414100000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William Land Regional Park (Freeport)", latitude: 38.537741716164600, longitude: -121.502256414100000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William Land Regional Park (Park Center)", latitude: 38.539551747380600, longitude: -121.500599418232000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William Land Regional Park (13th Avenue)", latitude: 38.544788078995300, longitude: -121.503195796494000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William Land Regional Park (11th Avenue)", latitude: 38.54579502244980, longitude: -121.505620513450000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Fredrick Miller Regional Park (Marina View drive)", latitude: 38.563338741966000, longitude: -121.518559489816000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Fredrick Miller Regional Park (Ramp Way)", latitude: 38.56040250230390, longitude: -121.515512501240000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Southside Park", latitude: 38.568852796584900, longitude: -121.501976081778000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Tiscornia Park", latitude: 38.597933390429300, longitude: -121.506440401269000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "William McKinley Park", latitude: 38.576445589709500, longitude: -121.462178588256000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "River Park", latitude: 38.5841994674306, longitude: -121.448982119414000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Tahoe Tallac Park", latitude: 38.544783654917000, longitude: -121.418482035757000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Tahoe Park", latitude: 38.543180908499200, longitude: -121.43555429534700)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Colonial Park", latitude: 38.534503337426100, longitude: -121.443803719981000)
 bathroom.save
