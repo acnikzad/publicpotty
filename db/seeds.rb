d90b3340214ea8bfcb201d5275a3ddd60dc78617
“Aria <a.c.nikzad@gmail.com>
2020-02-18 16:35:17 -0800

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,8 +79,83 @@
 # bathroom = Bathroom.create(park: "Meadowview Park", latitude: 38.474641392983400, longitude: -121.480931853365000)
 # bathroom.save
 
+bathroom = Bathroom.create(park: "Fredrick Miller Regional Park", latitude: 38.566994636784600, longitude: -121.518700421453000)
+bathroom.save
 
+bathroom = Bathroom.create(park: "Natomas Oaks Park", latitude: 38.605329385069000, longitude: -121.517460247346000)
+bathroom.save
 
+bathroom = Bathroom.create(park: "North Laguna Creek Park", latitude: 38.442987802614800, longitude: -121.428112712708000)
+bathroom.save
 
-bathroom = Bathroom.create(park: "Meadowview Park", latitude: 38.474641392983400, longitude: -121.480931853365000)
+bathroom = Bathroom.create(park: "North Natomas Community Park", latitude: 38.665949242888300, longitude: -121.503311273692000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Northgate Park", latitude: 38.616763351582500, longitude: -121.489368604894000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Regency Community Park", latitude: 38.674713495838200, longitude: -121.503363540492000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Joseph Reichmuth Park", latitude: 38.515361429482200, longitude: 38.515361429482200)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Charles Robertson Park", latitude: 38.63125757723530, longitude: -121.458695979836000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Shasta Community Park", latitude: 38.452248091687800, longitude: -121.417310768061000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Tanzanite Community Park", latitude: 38.632064419769600, longitude: -121.51138350895500)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Westlake Community Park", latitude: 38.657280101956100, longitude: -121.545194671627000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Witter Ranch Park", latitude: 38.638457821447500, longitude: -121.530626491219000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Wood Park", latitude: 38.466803584574700, longitude: -121.421585064641000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Woodbine Park", latitude: 38.504063665421400, longitude: -121.481968022061000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Glenbrook Park", latitude: 38.559591713608400, longitude: -121.390657023343000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "North Natomas Regional Park", latitude: 38.661581932063500, longitude: -121.518374294993000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Magoichi Oki Park", latitude: 38.555041873121500, longitude: -121.390154473511000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Theodore Roosevelt Park", latitude: 38.57346763609000, longitude: -121.49760095864100)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Southside Park", latitude: 38.568282583061600, longitude: -121.50378654881400)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Leland Stanford Park", latitude: 38.568282583061600, longitude: -121.50378654881400)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Tahoe Park", latitude: 38.542811580455800, longitude: -121.436270694728000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "South Natomas Community Park", latitude: 38.618708542241300, longitude: -121.499239187024000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Belle Cooledge Community Center Park", latitude: 38.52410456382670, longitude: -121.510795446551000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Coloma Park", latitude: 38.556784848145600, longitude: -121.450867432441000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Hagginwood Park", latitude: 38.627939332529700, longitude: -121.432764635447000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "Pannell/Meadowview Community Center Park", latitude: 38.48079025140130, longitude: -121.478837466451000)
+bathroom.save
+
+bathroom = Bathroom.create(park: "South Natomas Community Park", latitude: 38.619224487400700, longitude: -121.499310198925000)
 bathroom.save
